103c05aed696e58aa562a8a321e0f6a6fd855aec
Xica Felix <xicasfelix@gmail.com>
2022-12-02 16:00:39 -0500

    commit tree cc35868066303fd38957d34bd14e85d50c282969

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon WHERE id = ?
+        SQL
+        poke_data = db.execute(sql, id).flatten
+        Pokemon.new(id: poke_data[0], name: poke_data[1], type: poke_data[2], db: db)
+    end
 end
